aae6d76655150896165cb732645ad7d8a65e3600
Raoul1996 <baobaozhou199606@gmail.com>
2017-11-27 00:14:05 +0800

    update sy19

--- sy19.m
+++ sy19.m
@@ -1,8 +1,19 @@
 % x(n) = 2n + 5 (-4 <= n <= -1)
 % x(n) = 6 (0 <= n <= 4)
 % x(n) = 0 ÆäËû
-n = -20:20;
-U1 =6;
-x= (2 * n + 5).*(n >= -4 & n <= -1)+U1.*(n >= 0 & n <= 4);
-plot(n,x)
-axis([-21,21,-15,15])
+n1 = -4 : -1;
+x1 = 2 * n1 +5;
+n2 = 0 : 4;
+x2 = 6;
+n = -10 : 10;
+N=length(n);
+y1=zeros(1,N);
+y2=zeros(1,N); 
+y1(find((n>=min(n1))&(n<=max(n1))))=x1;
+y2(find((n>=min(n2))&(n<=max(n2))))=x2;
+y=y1+y2; 
+subplot(2,1,1);plot(n,y); 
+axis([-10 10 1.1*min(y) 1.1*max(y)]); ylabel('x(t)'); 
+subplot(2,1,2);stem(n,y,'filled'); 
+axis([-10 10 1.1*min(y) 1.1*max(y)])
+ylabel('x(n)');
