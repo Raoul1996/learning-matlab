48339d6e2112ff378dbbc7d3c2b3507481d2a968
Raoul1996 <baobaozhou199606@gmail.com>
2017-11-26 23:58:22 +0800

    增加运行结果

--- sy13.m
+++ sy13.m
@@ -2,11 +2,11 @@
 n1 = 16; a = 0.1;w = 1.6 * pi;
 n = 0 : n1;                                                                       
 x = exp((a + 1i * w) * n); 
-% subplot(2, 2, 1), plot(n, real(x)); 
-% title('复指数原信号的实部'); 
-subplot(2, 1, 1), stem(n, real(x), 'filled'); 
+subplot(2, 2, 1), plot(n, real(x)); 
 title('复指数原信号的实部'); 
-% subplot(2, 2, 3), plot(n, imag(x)); 
-% title('复指数原信号的虚部'); 
-subplot(2, 1, 2), stem(n, imag(x), 'filled'); 
+subplot(2, 2, 2), stem(n, real(x), 'filled'); 
+title('复指数原信号的实部'); 
+subplot(2, 2, 3), plot(n, imag(x)); 
+title('复指数原信号的虚部'); 
+subplot(2, 2, 4), stem(n, imag(x), 'filled'); 
 title('复指数原信号的虚部');
